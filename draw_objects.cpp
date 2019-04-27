cfa14f95fbf0b63845bad94ef87625d2dcebbe1c
Andrew J. Pounds <pounds_aj@mercer.edu>
2019-04-27 10:35:30 -0400

    changed the color of the vertical stand to green

--- draw_objects.cpp
+++ draw_objects.cpp
@@ -250,7 +250,8 @@ void draw_pendulum(){
 	//draw stand
 	glPushMatrix();
 //	glColor3f(.60784314, 1.0, .85490196);
-	glColor3f(0.0, 0.0, 1.0);
+    // Changed color to green for testing
+	glColor3f(0.0, 1.0, 0.0);
 	glTranslated(-1.0, 0.0, -0.55);
 	//glTranslated(-.866025404, 0.0, 0.0);
 	//glTranslated(0.0, 0.5, 0.0);
